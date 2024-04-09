c05bebd51c63e441c971db037aa5ff5b9eb878c7
Nandhakumar Ramanujan <nandha88@gmail.com>
2024-04-09 09:48:02 +0530

    Spring io startup

---
+++ src/test/java/com/nans/practice/PracticeApplicationTests.java
@@ -0,0 +1,14 @@
+package com.nans.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+		// this is empty as it is out of spring io startup
+	}
+
+}
